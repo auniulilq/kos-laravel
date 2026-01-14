ad0a7c51b001a0f6191b4b537c2f24caeace731c
auniulilq <auniulilqisty354@gmail.com>
2026-01-14 15:48:57 +0700

    Fix: point vite to sass instead of css

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -10,7 +10,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet">
 
-@vite(['resources/css/app.css', 'resources/js/app.js'])    
+@vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <style>
         :root {
             --primary: #3b82f6;
